f51d41614a5a9b06768f8de5f5a1187d7f635406
Dakshitha Hasindra <hasindradh@gmail.com>
2023-12-04 18:29:56 +0530

    Create post service

--- src/main/java/lk/ijse/dep11/edupanel/WebRootConfig.java
+++ src/main/java/lk/ijse/dep11/edupanel/WebRootConfig.java
@@ -1,9 +1,52 @@
 package lk.ijse.dep11.edupanel;
 
-import org.springframework.context.annotation.ComponentScan;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.core.env.Environment;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class WebRootConfig {
+    @Bean(destroyMethod = "close")
+    public HikariDataSource dataSource(Environment env){
+        HikariConfig hikariConfig = new HikariConfig();
+//        hikariConfig.setJdbcUrl(env.getRequiredProperty("spring.datasource.url"));
+        hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/dep11_edupanel");
+//        hikariConfig.setUsername(env.getRequiredProperty("spring.datasource.username"));
+        hikariConfig.setUsername("root");
+        hikariConfig.setPassword("7575");
+//        hikariConfig.setDriverClassName(env.getRequiredProperty("spring.datasource.driver-class"));
+        hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        hikariConfig.setMaximumPoolSize(env.getRequiredProperty("spring.datasource.hikari.maximum-pool-size", Integer.class));
+        hikariConfig.setMaximumPoolSize(10);
+        return new HikariDataSource(hikariConfig);
+    }
+
+
+    @Bean
+    public Bucket defaultBucket() throws IOException {
+//        FileInputStream serviceAccount =
+//                new FileInputStream("path/to/serviceAccountKey.json");
+
+        InputStream serviceAccount = getClass().getResourceAsStream("edu-panel-e7a92-firebase-adminsdk-t4rpq-3c01dfcfe3.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket("edu-panel-e7a92.appspot.com")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+        Bucket bucket = StorageClient.getInstance().bucket();
+        return bucket;
+    }
 }
--- src/main/java/lk/ijse/dep11/edupanel/api/LecturerHttpController.java
+++ src/main/java/lk/ijse/dep11/edupanel/api/LecturerHttpController.java
@@ -1,21 +1,95 @@
 package lk.ijse.dep11.edupanel.api;
 
+import com.google.api.services.storage.model.Bucket;
 import lk.ijse.dep11.edupanel.to.request.LecturerRequestTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.sql.DataSource;
 import javax.validation.Valid;
+import javax.xml.crypto.Data;
+import java.sql.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/v1/lecturers")
 public class LecturerHttpController {
 
+    @Autowired
+    private DataSource pool;
+
+    @Autowired
+    private Bucket bucket;
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = "multipart/form-data", produces = "application/json")
     public void createNewLecturer( @ModelAttribute @Valid LecturerRequestTO lecturer){
+        try (Connection connection = pool.getConnection()){
+
+            /* Transaction mode */
+            connection.setAutoCommit(false);
+            try {
+                PreparedStatement stm = connection.prepareStatement("INSERT INTO lecturer ( name, designation, qualifications, linkedin) VALUES (?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+
+                stm.setString(1,lecturer.getName());
+                stm.setString(2,lecturer.getDesignation());
+                stm.setString(3,lecturer.getQualifications());
+                stm.setString(4,lecturer.getLinkedIn());
+                stm.executeUpdate();
+                ResultSet generatedKeys = stm.getGeneratedKeys();
+                generatedKeys.next();
+                int lecturerID = generatedKeys.getInt(1);
+
+                /*Picture name*/
+                String picture = lecturerID + "-" +lecturer.getName();
+
+                /* Save picture data in database */
+                if(lecturer.getPicture() != null || !lecturer.getPicture().isEmpty()) {
+                    System.out.println("Picture have");
+                    PreparedStatement stmPicture = connection.prepareStatement("UPDATE lecturer SET picture=? WHERE id=?");
+                    stmPicture.setString(1,picture);
+                    stmPicture.setInt(2,lecturerID);
+                    stmPicture.executeUpdate();
+                }
+
+                /* Insert data in to full-time table or part-time table*/
+                final String table = lecturer.getType().equalsIgnoreCase("full-time")?"full_time_rank":"part_time_rank";
+
+                    Statement stmGetRank = connection.createStatement();
+                    ResultSet rst = stmGetRank.executeQuery("SELECT `rank` FROM "+table+" ORDER BY  `rank` DESC LIMIT 1");
+
+                    int newRank ;
+
+
+                    if(!rst.next()){
+                        newRank =1;
+                    }else {
+                        newRank = rst.getInt(1)+1;
+                    }
+                System.out.println(newRank);
+                    PreparedStatement stmFullOrPartTime = connection.prepareStatement("INSERT INTO "+table+" (lecture_id, `rank`) VALUES (?,?)");
+                    stmFullOrPartTime.setInt(1,lecturerID);
+                    stmFullOrPartTime.setInt(2,newRank);
+                    stmFullOrPartTime.executeUpdate();
+
+
+
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
+                throw new RuntimeException(e);
+            } finally {
+                connection.setAutoCommit(true);
+            }
+
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         System.out.println(lecturer);
         System.out.println("Save this lecturer");
     }
